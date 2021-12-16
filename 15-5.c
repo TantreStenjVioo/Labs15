4becaa41a50c4e621b8e1c0b95ff5ec00a2262a2
TantreStenjVioo <96138379+TantreStenjVioo@users.noreply.github.com>
2021-12-16 04:41:15 +0300

    Rename 15.5.c to 15-5.c

--- 15-5.c
+++ 15-5.c
@@ -1,7 +1,7 @@
 #include<math.h>
 #include <stdio.h>
 
-float fact2(int n)
+float fact2(int n) // Создаем функцию по заданным условиям
 {
     int temp = 1;
     for (; n > 0; n -= 2) temp *= n;
@@ -15,7 +15,7 @@ int main(void)
 	{
         printf("N:");
         scanf("%i", &n);
-        printf("fact: %f\n", fact2(n));
+        printf("fact: %f\n", fact2(n)); // Вызываем функцию и выводим результат
     }
     return 0;
 }
